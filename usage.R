fca6f8b293568219b5fa3a9c63b8d7c163ca20e8
gwangil <tjtjsksk123@naver.com>
2019-03-05 17:32:17 +0900

    upload two functions and modify old

--- usage.R
+++ usage.R
@@ -1,7 +1,7 @@
 sapply(paste0("./functions/", list.files("./functions")), source)
 
 getSummoner("신뢰구간")
-getRankTier("신뢰구간")
+getTier("신뢰구간")
 getMatchHistory("신뢰구간")
 getChampionMastery("신뢰구간")
 getTotalMasteryScore("신뢰구간")
