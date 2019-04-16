b2cc54f54ecb1e23cf0f3eaf839c0a011a07ac22
Gwangil <tjtjsksk123@naver.com>
2019-04-16 16:18:49 +0900

    update global glod diff. and movement ARAM game

--- C.I.GG.R
+++ C.I.GG.R
@@ -1,16 +1,4 @@
 sapply(paste0("./functions/", list.files("./functions")), source)
-if (!"shiny" %in% installed.packages()) {
-  install.packages("shiny")
-  library(shiny)
-} else {library(shiny)}
-if (!"DT" %in% installed.packages()) {
-  install.packages("DT")
-  library(DT)
-} else {library(DT)}
-if (!"shinyjs" %in% installed.packages()) {
-  install.packages("shinyjs")
-  library(shinyjs)
-} else {library(shinyjs)}
 
 ui <- fluidPage(
   shinyjs::useShinyjs(),
@@ -34,21 +22,24 @@ ui <- fluidPage(
     tabPanel("챔피언 숙련도",  fluidRow(column(width = 12,
                                          shinyjs::hidden(imageOutput(outputId = "veteran", width = "100%", height = "100%")),
                                          DTOutput("championMastery"),
-                                         shinyjs::hidden(downloadButton(outputId = 'downMastery', label = 'DownloadMastery'))
-    )
-    )
-    ),
+                                         shinyjs::hidden(downloadButton(outputId = 'downMastery', label = 'DownloadMastery'))))),
     tabPanel("최근 게임", fluidRow(column(width = 12,
                                       DTOutput("matchHistory"),
                                       shinyjs::hidden(downloadButton(outputId = 'downHistory', label = 'DownloadHistory'))))),
-    tabPanel("최근 협곡 동선", fluidRow(column(width = 12,
+    tabPanel("최근 게임 동선", fluidRow(column(width = 12,
                                          column(width = 4,
                                                 shinyjs::hidden(plotOutput(outputId = "playingPath"))),
                                          column(width = 4,
                                                 shinyjs::hidden(plotOutput(outputId = "playingPath2"))),
                                          column(width = 4,
-                                                shinyjs::hidden(plotOutput(outputId = "playingPath3")))))
-    )),
+                                                shinyjs::hidden(plotOutput(outputId = "playingPath3")))))),
+    tabPanel("최근 게임 골드차이", fluidRow(column(width = 12,
+                                           column(width = 4,
+                                                  plotlyOutput(outputId = "globalGold")),
+                                           column(width = 4,
+                                                  plotlyOutput(outputId = "globalGold2")),
+                                           column(width = 4,
+                                                  plotlyOutput(outputId = "globalGold3")))))),
   
   fluidRow(column(2, h1("C.I.GG")),
            column(3, h1("Made by Gwangil")),
@@ -104,7 +95,7 @@ server <- function(input, output) {
       
       png(outfile, width = 1000, height = 200)
       par(mfrow = c(1, length(url_image)))
-      sapply(url_image, function(x) countcolors::plotArrayAsImage(GET(url = x) %>% content))
+      sapply(url_image, function(x) {countcolors::plotArrayAsImage(GET(url = x) %>% content)})
       mtext("Top 10 of veteran champions", at = -5, line = 0, cex = 2)
       dev.off()
       
@@ -135,89 +126,198 @@ server <- function(input, output) {
                                             write.csv(gotHistory[input$matchHistory_rows_all, , drop = T], file, row.names = F, fileEncoding = "UTF-8")
                                           })
     
-    if (!(gotHistory$게임종류[1] %>% str_detect("ARAM"))) {
-      output$playingPath <- renderPlot({
-        GET(url = paste0("https://kr.api.riotgames.com/lol/match/v4/timelines/by-match/", gotHistory$gameId[1]),
-            add_headers("X-Riot-Token" = getOption("RiotApiKey"))) %>% content %>% `[[`("frames") %>%
-          lapply(`[[`, "participantFrames") %>% unlist() %>% enframe() %>%
-          filter(str_detect(name, paste0(gotHistory$participantNo[1], ".position"))) %>%
-          pull(value) %>% matrix(ncol = 2, byrow = T) %>% as.tibble() %>% `colnames<-`(c("x", "y")) %>%
-          ggplot(mapping = aes(x = x, y = y)) +
-          annotation_custom(summonersLift) +
+    timeline1 <- GET(url = paste0("https://kr.api.riotgames.com/lol/match/v4/timelines/by-match/", gotHistory$gameId[1]),
+                     add_headers("X-Riot-Token" = getOption("RiotApiKey"))) %>% content %>% unlist() %>% enframe()
+    timeline2 <- GET(url = paste0("https://kr.api.riotgames.com/lol/match/v4/timelines/by-match/", gotHistory$gameId[2]),
+                     add_headers("X-Riot-Token" = getOption("RiotApiKey"))) %>% content %>% unlist() %>% enframe()
+    timeline3 <- GET(url = paste0("https://kr.api.riotgames.com/lol/match/v4/timelines/by-match/", gotHistory$gameId[3]),
+                     add_headers("X-Riot-Token" = getOption("RiotApiKey"))) %>% content %>% unlist() %>% enframe()
+    
+    output$playingPath <- renderPlot({
+      g <- timeline1 %>% filter(str_detect(name, paste0(gotHistory$participantNo[1], ".position"))) %>%
+        pull(value) %>% as.integer() %>% matrix(ncol = 2, byrow = T, dimnames = list(NULL, c("x", "y"))) %>% as.tibble() %>%
+        ggplot(mapping = aes(x = x, y = y))
+      
+      if(gotHistory$게임종류[1] %>% str_detect("ARAM")) {
+        g <- g +
+          annotation_custom(howlingAbyss) +
+          coord_fixed(xlim = c(-28, 12849), ylim = c(-19, 12858), ratio = 1, expand = F) +
           annotation_custom(grid::rasterGrob(GET(url = paste0(getOption("DDragon"),
                                                               getOption("LOLPatch"),"/img/champion/",
                                                               championImageFile[[championId %>%
                                                                                    filter(championNameKo == gotHistory$챔피언[1]) %>%
                                                                                    pull(championName)]])) %>% content, interpolate = T),
-                            xmin = 0, xmax = 1800, ymin = 13200, ymax = 15000) + 
-          geom_path(aes(size = 1.5, colour = "red")) +
-          coord_fixed(xlim = c(-120, 14870), ylim = c(-120, 14980), ratio = 1, expand = F) +
-          ggtitle("Movement in the most recently played game") +
-          theme(axis.title = element_blank(),
-                axis.line = element_blank(),
-                axis.text = element_blank(),
-                axis.ticks = element_blank(),
-                legend.position = "none",
-                panel.grid = element_blank()) 
-      })
-      shinyjs::show("playingPath")
-    }
-    
-    if (!(gotHistory$게임종류[2] %>% str_detect("ARAM"))) {
-      output$playingPath2 <- renderPlot({
-        GET(url = paste0("https://kr.api.riotgames.com/lol/match/v4/timelines/by-match/", gotHistory$gameId[2]),
-            add_headers("X-Riot-Token" = getOption("RiotApiKey"))) %>% content %>% `[[`("frames") %>%
-          lapply(`[[`, "participantFrames") %>% unlist() %>% enframe() %>%
-          filter(str_detect(name, paste0(gotHistory$participantNo[2], ".position"))) %>%
-          pull(value) %>% matrix(ncol = 2, byrow = T) %>% as.tibble() %>% `colnames<-`(c("x", "y")) %>%
-          ggplot(mapping = aes(x = x, y = y)) +
+                            xmin = 0, xmax = 1800, ymin = 11200)
+      } else {
+        g <- g +
           annotation_custom(summonersLift) +
+          coord_fixed(xlim = c(-120, 14870), ylim = c(-120, 14980), ratio = 1, expand = F) +
+          annotation_custom(grid::rasterGrob(GET(url = paste0(getOption("DDragon"),
+                                                              getOption("LOLPatch"),"/img/champion/",
+                                                              championImageFile[[championId %>%
+                                                                                   filter(championNameKo == gotHistory$챔피언[1]) %>%
+                                                                                   pull(championName)]])) %>% content, interpolate = T),
+                            xmin = 0, xmax = 1800, ymin = 13200)
+      }
+      g +
+        geom_path(aes(size = 1.5, colour = "red")) +
+        ggtitle("Movement in the most recent game") +
+        theme(axis.title = element_blank(),
+              axis.line = element_blank(),
+              axis.text = element_blank(),
+              axis.ticks = element_blank(),
+              legend.position = "none",
+              panel.grid = element_blank())
+    })
+    shinyjs::show("playingPath")
+    
+    output$playingPath2 <- renderPlot({
+      g <- timeline2 %>% filter(str_detect(name, paste0(gotHistory$participantNo[2], ".position"))) %>%
+        pull(value) %>% as.integer() %>% matrix(ncol = 2, byrow = T, dimnames = list(NULL, c("x", "y"))) %>% as.tibble() %>%
+        ggplot(mapping = aes(x = x, y = y))
+      
+      if(gotHistory$게임종류[2] %>% str_detect("ARAM")) {
+        g <- g +
+          annotation_custom(howlingAbyss) +
+          coord_fixed(xlim = c(-28, 12849), ylim = c(-19, 12858), ratio = 1, expand = F) +
           annotation_custom(grid::rasterGrob(GET(url = paste0(getOption("DDragon"),
                                                               getOption("LOLPatch"),"/img/champion/",
                                                               championImageFile[[championId %>%
                                                                                    filter(championNameKo == gotHistory$챔피언[2]) %>%
                                                                                    pull(championName)]])) %>% content, interpolate = T),
-                            xmin = 0, xmax = 1800, ymin = 13200, ymax = 15000) + 
-          geom_path(aes(size = 1.5, colour = "red")) +
-          coord_fixed(xlim = c(-120, 14870), ylim = c(-120, 14980), ratio = 1, expand = F) +
-          ggtitle("Movement in the 2nd recently played game") +
-          theme(axis.title = element_blank(),
-                axis.line = element_blank(),
-                axis.text = element_blank(),
-                axis.ticks = element_blank(),
-                legend.position = "none",
-                panel.grid = element_blank())
-      })
-      shinyjs::show("playingPath2")
-    }
-    
-    if (!(gotHistory$게임종류[3] %>% str_detect("ARAM"))) {
-      output$playingPath3 <- renderPlot({
-        GET(url = paste0("https://kr.api.riotgames.com/lol/match/v4/timelines/by-match/", gotHistory$gameId[3]),
-            add_headers("X-Riot-Token" = getOption("RiotApiKey"))) %>% content %>% `[[`("frames") %>%
-          lapply(`[[`, "participantFrames") %>% unlist() %>% enframe() %>%
-          filter(str_detect(name, paste0(gotHistory$participantNo[3], ".position"))) %>%
-          pull(value) %>% matrix(ncol = 2, byrow = T) %>% as.tibble() %>% `colnames<-`(c("x", "y")) %>%
-          ggplot(mapping = aes(x = x, y = y)) +
+                            xmin = 0, xmax = 1800, ymin = 11200)
+      } else {
+        g <- g +
           annotation_custom(summonersLift) +
+          coord_fixed(xlim = c(-120, 14870), ylim = c(-120, 14980), ratio = 1, expand = F) +
+          annotation_custom(grid::rasterGrob(GET(url = paste0(getOption("DDragon"),
+                                                              getOption("LOLPatch"),"/img/champion/",
+                                                              championImageFile[[championId %>%
+                                                                                   filter(championNameKo == gotHistory$챔피언[2]) %>%
+                                                                                   pull(championName)]])) %>% content, interpolate = T),
+                            xmin = 0, xmax = 1800, ymin = 13200)
+      }
+      g +
+        geom_path(aes(size = 1.5, colour = "red")) +
+        ggtitle("Movement in the 2nd most recent game") +
+        theme(axis.title = element_blank(),
+              axis.line = element_blank(),
+              axis.text = element_blank(),
+              axis.ticks = element_blank(),
+              legend.position = "none",
+              panel.grid = element_blank())
+    })
+    shinyjs::show("playingPath2")
+    
+    output$playingPath3 <- renderPlot({
+      g <- timeline3 %>% filter(str_detect(name, paste0(gotHistory$participantNo[3], ".position"))) %>%
+        pull(value) %>% as.integer() %>% matrix(ncol = 2, byrow = T, dimnames = list(NULL, c("x", "y"))) %>% as.tibble() %>%
+        ggplot(mapping = aes(x = x, y = y))
+      
+      if(gotHistory$게임종류[3] %>% str_detect("ARAM")) {
+        g <- g +
+          annotation_custom(howlingAbyss) +
+          coord_fixed(xlim = c(-28, 12849), ylim = c(-19, 12858), ratio = 1, expand = F) +
           annotation_custom(grid::rasterGrob(GET(url = paste0(getOption("DDragon"),
                                                               getOption("LOLPatch"),"/img/champion/",
                                                               championImageFile[[championId %>%
                                                                                    filter(championNameKo == gotHistory$챔피언[3]) %>%
                                                                                    pull(championName)]])) %>% content, interpolate = T),
-                            xmin = 0, xmax = 1800, ymin = 13200, ymax = 15000) + 
-          geom_path(aes(size = 1.5, colour = "red")) +
+                            xmin = 0, xmax = 1800, ymin = 11200)
+      } else {
+        g <- g +
+          annotation_custom(summonersLift) +
           coord_fixed(xlim = c(-120, 14870), ylim = c(-120, 14980), ratio = 1, expand = F) +
-          ggtitle("Movement in the 3rd recently played game") +
-          theme(axis.title = element_blank(),
-                axis.line = element_blank(),
-                axis.text = element_blank(),
-                axis.ticks = element_blank(),
-                legend.position = "none",
-                panel.grid = element_blank())
-      })
-      shinyjs::show("playingPath3")
-    }
+          annotation_custom(grid::rasterGrob(GET(url = paste0(getOption("DDragon"),
+                                                              getOption("LOLPatch"),"/img/champion/",
+                                                              championImageFile[[championId %>%
+                                                                                   filter(championNameKo == gotHistory$챔피언[3]) %>%
+                                                                                   pull(championName)]])) %>% content, interpolate = T),
+                            xmin = 0, xmax = 1800, ymin = 13200)
+      }
+      g +
+        geom_path(aes(size = 1.5, colour = "red")) +
+        ggtitle("Movement in the 3rd most recent game") +
+        theme(axis.title = element_blank(),
+              axis.line = element_blank(),
+              axis.text = element_blank(),
+              axis.ticks = element_blank(),
+              legend.position = "none",
+              panel.grid = element_blank())
+    })
+    shinyjs::show("playingPath3")
+    
+    output$globalGold <- renderPlotly({
+      g <- timeline1 %>% filter(str_detect(name, "totalGold")) %>%
+        pull(value) %>% as.integer() %>% matrix(ncol = 10, byrow = T) %>% as_tibble() %>%
+        mutate(mins = row_number(),
+               blueGold = rowSums(.[1:5]),
+               redGold = rowSums(.[6:10]),
+               diffGold = blueGold - redGold,
+               above.h = diffGold >= 0,
+               changed = is.na(lag(above.h)) | lag(above.h) != above.h,
+               section.id = cumsum(changed)) %>%
+        select(mins, diffGold, section.id) %>%
+        rbind(conversionPoint(.)) %>%
+        ggplot(data = ., aes(x = mins)) +
+        geom_ribbon(data = . %>% filter(diffGold >= 0),
+                    aes(ymin = 0, ymax = diffGold), fill = "skyblue") +
+        geom_ribbon(data = . %>% filter(diffGold <= 0),
+                    aes(ymin = diffGold, ymax = 0), fill = "hotpink") +
+        geom_point(data = . %>% filter(diffGold != 0),
+                   aes(y = diffGold)) +
+        ggtitle(gotHistory$챔피언[1] %>% as.character()) +
+        theme_void() +
+        expand_limits(y = 500)
+      ggplotly(g)})
+    
+    output$globalGold2 <- renderPlotly({
+      g <- timeline2 %>% filter(str_detect(name, "totalGold")) %>%
+        pull(value) %>% as.integer() %>% matrix(ncol = 10, byrow = T) %>% as_tibble() %>%
+        mutate(mins = row_number(),
+               blueGold = rowSums(.[1:5]),
+               redGold = rowSums(.[6:10]),
+               diffGold = blueGold - redGold,
+               above.h = diffGold >= 0,
+               changed = is.na(lag(above.h)) | lag(above.h) != above.h,
+               section.id = cumsum(changed)) %>%
+        select(mins, diffGold, section.id) %>%
+        rbind(conversionPoint(.)) %>%
+        ggplot(data = ., aes(x = mins)) +
+        geom_ribbon(data = . %>% filter(diffGold >= 0),
+                    aes(ymin = 0, ymax = diffGold), fill = "skyblue") +
+        geom_ribbon(data = . %>% filter(diffGold <= 0),
+                    aes(ymin = diffGold, ymax = 0), fill = "hotpink") +
+        geom_point(data = . %>% filter(diffGold != 0),
+                   aes(y = diffGold)) +
+        ggtitle(gotHistory$챔피언[2] %>% as.character()) +
+        theme_void() +
+        expand_limits(y = 500)
+      ggplotly(g)})
+    
+    output$globalGold3 <- renderPlotly({
+      g <- timeline3 %>% filter(str_detect(name, "totalGold")) %>%
+        pull(value) %>% as.integer() %>% matrix(ncol = 10, byrow = T) %>% as_tibble() %>%
+        mutate(mins = row_number(),
+               blueGold = rowSums(.[1:5]),
+               redGold = rowSums(.[6:10]),
+               diffGold = blueGold - redGold,
+               above.h = diffGold >= 0,
+               changed = is.na(lag(above.h)) | lag(above.h) != above.h,
+               section.id = cumsum(changed)) %>%
+        select(mins, diffGold, section.id) %>%
+        rbind(conversionPoint(.)) %>%
+        ggplot(data = ., aes(x = mins)) +
+        geom_ribbon(data = . %>% filter(diffGold >= 0),
+                    aes(ymin = 0, ymax = diffGold), fill = "skyblue") +
+        geom_ribbon(data = . %>% filter(diffGold <= 0),
+                    aes(ymin = diffGold, ymax = 0), fill = "hotpink") +
+        geom_point(data = . %>% filter(diffGold != 0),
+                   aes(y = diffGold)) +
+        ggtitle(gotHistory$챔피언[3] %>% as.character()) +
+        theme_void() +
+        expand_limits(y = 500)
+      ggplotly(g)})
     
     shinyjs::show("veteran")
     shinyjs::show("downMastery")
@@ -225,7 +325,6 @@ server <- function(input, output) {
     shinyjs::enable("go")
     shinyjs::hide("btnText")
   })
-  
 }
 
 shinyApp(ui, server)
--- functions/env.R
+++ functions/env.R
@@ -14,6 +14,22 @@ if (!"countcolors" %in% installed.packages()) {
   install.packages("countcolors")
   library(countcolors)
 } else {library(countcolors)}
+if (!"shiny" %in% installed.packages()) {
+  install.packages("shiny")
+  library(shiny)
+} else {library(shiny)}
+if (!"shinyjs" %in% installed.packages()) {
+  install.packages("shinyjs")
+  library(shinyjs)
+} else {library(shinyjs)}
+if (!"DT" %in% installed.packages()) {
+  install.packages("DT")
+  library(DT)
+} else {library(DT)}
+if (!"plotly" %in% installed.packages()) {
+  install.packages("plotly")
+  library(plotly)
+} else {library(plotly)}
 
 options(RiotApiKey = "YOUR_RIOT_API_KEY") # https://developer.riotgames.com
 options(LOLPatch = read_json("https://ddragon.leagueoflegends.com/api/versions.json")[[1]]) # LOL patch version
@@ -42,4 +58,18 @@ runeCore <- do.call(bind_rows,
 queueType <- suppressWarnings(read_tsv("queueType.txt")[, 1:3])
 
 summonersLift <- grid::rasterGrob(GET(url = "https://s3-us-west-1.amazonaws.com/riot-developer-portal/docs/map11.png") %>% content, interpolate = T)
-# howlingAbyss <- grid::rasterGrob(GET(url = "https://s3-us-west-1.amazonaws.com/riot-developer-portal/docs/map12.png") %>% content, interpolate = T)
+howlingAbyss <- grid::rasterGrob(GET(url = "https://s3-us-west-1.amazonaws.com/riot-developer-portal/docs/map12.png") %>% content, interpolate = T)
+
+conversionPoint <- function(data) {data %>%
+    group_by(section.id) %>%
+    filter(mins %in% c(min(mins), max(mins))) %>%
+    ungroup() %>%
+    mutate(mid.x = ifelse(section.id == 1 | 
+                            section.id == lag(section.id), 
+                          NA,
+                          mins - (mins - lag(mins)) / 
+                            (diffGold - lag(diffGold)) * (diffGold - 0))) %>% 
+    select(mid.x, diffGold, section.id) %>%
+    rename(mins = mid.x) %>%
+    mutate(diffGold = 0) %>%
+    na.omit()}
